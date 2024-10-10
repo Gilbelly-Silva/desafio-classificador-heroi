3de6de41d730edb66033e185e39ba80953e0ed5c
Gilbelly Silva <gilbellykaren@gmail.com>
2024-10-09 22:27:00 -0300

    Alterando o nome do nível

--- nivel.js
+++ nivel.js
@@ -1,19 +1,3 @@
-/*Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-Se XP for menor do que 1.000 = Ferro
-Se XP for entre 1.001 e 2.000 = Bronze
-Se XP for entre 2.001 e 5.000 = Prata
-Se XP for entre 5.001 e 7.000 = Ouro
-Se XP for entre 7.001 e 8.000 = Platina
-Se XP for entre 8.001 e 9.000 = Ascendente
-Se XP for entre 9.001 e 10.000= Imortal
-Se XP for maior ou igual a 10.001 = Radiante
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói de nome **{nome}** está no nível de **{nivel}**" */
-
 let nome = "Jogador 1";
 let XP= 7500;
 let nivel;
@@ -33,7 +17,7 @@ if(XP <= 1000){
     }else if(XP >= 9001 && XP <= 10000){
         nivel= "Imortal";
     }else if (XP >= 10001){
-        nivel= "Imortal";
+        nivel= "Radiante";
     }      
         
 console.log(`O Herói de nome ${nome} está no nível de ${nivel}.`)
